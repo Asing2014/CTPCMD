fbcb0972c90484333a28420cad59ebc3b6403aa1
Asing <mi_sun@foxmail.com>
2019-07-17 18:13:04 +0800

    2017-07-17

---
+++ 2019-07-17/MD.bat
@@ -0,0 +1,5 @@
+@TITLE 时间  毫秒  最新价  现量  增仓  成交量  持仓量  本地时间  本地毫秒
+@fltmc>nul||cd/d %~dp0&&mshta vbscript:CreateObject("Shell.Application").ShellExecute("%~nx0","%1","","runas",1)(window.close)&&exit
+@cd C:\QH\MD
+@CTPMDTrader.exe 1 sc1909 200
+@echo off
